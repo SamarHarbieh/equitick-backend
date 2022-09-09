f78f3782e6025162c13e941f1d1e8344a3e28a2c
Samar Harbieh <samarharbieh.sh@gmail.com>
2022-09-09 09:08:55 +0300

    clientS

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\Trade;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/trades', function(){
+    return Trade::all();
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
