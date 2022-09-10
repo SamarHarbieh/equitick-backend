82f1024e5dbdf15c13135246804e027786b5554f
Samar Harbieh <samarharbieh.sh@gmail.com>
2022-09-10 11:46:45 +0300

    Completed user auth

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\TradesController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,21 +8,23 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
-Route::get('/trades', [TradesController::class, 'index']);
-// Route::get('/trades/{Deal}',[TradesController::class, 'show']);
-Route::post('/trades', [TradesController::class, 'store']);
-Route::put('/trades/{Deal}',[TradesController::class,'update']);
-Route::delete('/trades/{Deal}',[TradesController::class, 'destroy']);
 
-// Route::resource('trades', TradesController::class);
+// Public routes
+    // Register route
+Route::post('/register', [AuthController::class, 'register']);
+    // Login route
+Route::post('/login', [AuthController::class, 'login']);
 
-
+// Protected routes
 Route::group(['middleware' => ['auth:sanctum']], function(){
+    // Trades routes
+    Route::get('/trades', [TradesController::class, 'index']);
     Route::get('/trades/{Deal}',[TradesController::class, 'show']);
+    Route::post('/trades', [TradesController::class, 'store']);
+    Route::put('/trades/{Deal}',[TradesController::class,'update']);
+    Route::delete('/trades/{Deal}',[TradesController::class, 'destroy']);
+
+    // Logout
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
