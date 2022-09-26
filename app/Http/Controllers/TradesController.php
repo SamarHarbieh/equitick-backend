c7ef6050c57c20a400145c5e3e59ef8c5fd3ede8
samar <samarharbieh.sh@gmail.com>
2022-09-27 01:05:18 +0300

    fixed to find any occurenece not only statting deals or logins

--- app/Http/Controllers/TradesController.php
+++ app/Http/Controllers/TradesController.php
@@ -12,17 +12,40 @@ class TradesController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    // public function index(Request $request)
+    // {
+    //     if($request->Deal && $request->Login) {
+    //     return Trade::where('Deal','like',$request->Deal.'%')->where('Login',$request->Login)->orderBy('Time','desc')->paginate(10);
+    //     }
+    //     if($request->Deal){
+    //         return Trade::where('Deal','like',$request->Deal.'%')->paginate(10)->orderBy('Time','desc');
+    //     }
+    //     if($request->Login){
+    //         return Trade::where('Login', $request->Login)->orderBy('Time','desc')->paginate(10);
+    //     }
+    //     return Trade::orderBy('Time','desc')->paginate(10);
+    // }
+
+
+    //'%' . $word .'%'
     public function index(Request $request)
     {
-        if($request->Deal && $request->Login) {
-        return Trade::where('Deal','like',$request->Deal.'%')->where('Login','like',$request->Login.'%')->orderBy('Time','desc')->paginate(10);
+        if($request->DealFilter && $request->LoginFilter) {
+        return Trade::where('Deal','like','%' .$request->DealFilter.'%')->where('Login','like', '%' .$request->LoginFilter.'%')->orderBy('Time','desc')->paginate(10);
         }
-        if($request->Deal){
-            return Trade::where('Deal','like',$request->Deal.'%')->orderBy('Time','desc')->paginate(10);
+        if($request->DealFilter && $request->Login) {
+            return Trade::where('Deal','like','%' .$request->DealFilter.'%')->where('Login', $request->Login)->orderBy('Time','desc')->paginate(10);
+            }
+        if($request->DealFilter){
+            return Trade::where('Deal','like','%' .$request->DealFilter.'%')->orderBy('Time','desc')->paginate(10);
+        }
+        if($request->LoginFIlter){
+            return Trade::where('Login', 'like', '%' .$request->LoginFilter.'%')->orderBy('Time','desc')->paginate(10);
         }
         if($request->Login){
-            return Trade::where('Login','like',$request->Login.'%')->orderBy('Time','desc')->paginate(10);
+            return Trade::where('Login', $request->Login)->orderBy('Time','desc')->paginate(10);
         }
+
         return Trade::orderBy('Time','desc')->paginate(10);
     }
 
